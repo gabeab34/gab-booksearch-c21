79114afac59fc4acac1c438e0be1a00fc882b057
Gabriel Adams Bogar <gabeab34@gmail.com>
2022-09-30 11:35:56 -0400

    added initial typeDefs structure

---
+++ Develop/server/schemas/TypeDefs.js
@@ -0,0 +1,33 @@
+import {gql} from ("apollo-server-express")
+
+const typeDefs = gql`
+
+type Book {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+}
+
+type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+}
+
+type Auth {
+    token: String
+    user: User
+}
+
+type Query {
+    me: User
+}
+
+`;
+
+module.exports = { typeDefs }
